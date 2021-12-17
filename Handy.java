675de54eb4553211ebf6e7ce4e07a60d5ba6853c
churchmair <jonas1.kirchmair@gmail.com>
2021-12-17 13:04:37 +0100

    Handy.java nochmals erweitert

--- Handy.java
+++ Handy.java
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 public class Handy{
     private String _brand;
+    private int _handyID;
+    private String _name;
 }
-=======
-public class Handy{
-  private int _handyID;
-}
->>>>>>> 4111f60f60922f5b4e4088f367d56ac451eb614a
